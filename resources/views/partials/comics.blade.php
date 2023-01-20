32161c34d2c4eb29fe09e8374f812dbfc23c9b63
FrancescoGentile97 <francesco.gentile97@icloud.com>
2023-01-20 17:50:22 +0100

    aggiunta navbar e inclusa nel layout tramite yield

---
+++ resources/views/partials/comics.blade.php
@@ -0,0 +1,3 @@
+@extends("layout.app")
+
+<!-- fare una @section con un ciclo dove mostrare e stampare i fumetti e poi "innestarlo" dove serve -->
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,26 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// route della home
 Route::get('/', function () {
     return view("home");
 })->name("home");
+
+// route dei personaggi
+Route::get("/characters", function () {
+    return view("characters");
+})->name("characters");
+
+// route dei film
+
+Route::get("/movie", function () {
+    return view("movie");
+})->name("movie");
+
+// prova route per i fumetti
+Route::get("/comics", function(){
+    $comics = config("comics");
+    return view("partials.comics",[
+        "comicsList" => $comics
+    ]);
+})->name("comics");
