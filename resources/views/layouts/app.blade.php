c401fa0683281e54b2d697fae4835dc418cfa81b
FrancescoGentile97 <francesco.gentile97@icloud.com>
2023-01-20 15:51:09 +0100

    aggiunto primo layout e preparate partials per testare

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    @vite('resources/js/app.js')
+
+    <title>Laravel-Comics</title>
+</head>
+<body>
+<h1>Vediamo se funziona</h1>
+    
+</body>
+</html>
